a3a4749ae9fa905ab6392cf86e77391a0ecc4df0
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-06-16 12:17:17 +0200

    Add background image to App component and refactor layout

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,62 +1,71 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import { Button, TextInput } from 'react-native-web'
 import { getUser, getUserSummaries } from '../api/tetrioEndpoints'
+import image from '../../assets/tetrioBackground.jpg'
 
 export default function App({ navigation }) {
   return (
-    <View style={styles.container}>
-        <Text style={styles.title}>Tetrio Comparator</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder='user name'
-          placeholderTextColor={'#808080'}
-        />
-        <Button
-          title='USER INFO'
-          style={[styles.button, { marginTop: 60 }]}
-          onPress={() => {
-            console.log('Button pressed');
-          }}
-        />
-        <View style={styles.spacer}/>
-        <View style={styles.userComparator}>
-          <TextInput
-          style={styles.textInput}
-            placeholder='user1 name'
-            placeholderTextColor={'#808080'}
-          />
+    <ImageBackground source={image} style={{
+      width: '100%',
+      height: '100%'
+    }}>
+      <View style={styles.container}>
+          <Text style={styles.title}>Tetrio Comparator</Text>
           <TextInput
             style={styles.textInput}
-            placeholder='user2 name'
+            placeholder='user name'
             placeholderTextColor={'#808080'}
           />
-        </View>
-        <Button
-          title='COMPARE USERS'
-          style={styles.button}
-          onPress={() => {
-            console.log('Button pressed');
-          }}
-        />
-      <StatusBar style="dark" />
-    </View>
+          <View style={styles.spacerUser}/>
+          <Button
+            title='USER INFO'
+            style={[styles.button, { marginTop: 60 }]}
+            onPress={() => {
+              alert("Has perdido")
+            }}
+          />
+          <View style={styles.spacer}/>
+          <View style={styles.userComparator}>
+            <TextInput
+            style={styles.textInput}
+              placeholder='user1 name'
+              placeholderTextColor={'#808080'}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder='user2 name'
+              placeholderTextColor={'#808080'}
+            />
+          </View>
+          <Button
+            title='COMPARE USERS'
+            style={styles.button}
+            onPress={() => {
+              alert("Has perdido otra vez")
+            }}
+          />
+        <StatusBar style="dark" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#151314',
     alignItems: 'center',
     paddingTop: 80
   },
   userComparator: {
     margin: 20
   },
+  spacerUser: {
+    height: 20
+  },
   title: {
     color: 'white',
-    fontSize: 100,
+    fontSize: '528%',
     fontWeight: 'bold',
     marginBottom: 40,
   },
