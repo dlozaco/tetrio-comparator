ebedd9285e4b1de1f8de3b1d184f473ed8ac38ea
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-06-20 17:56:47 +0200

    Refactor App component layout and improve input field placeholders

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,50 +1,57 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { useFonts } from 'expo-font'
+import { Tiny5_400Regular } from '@expo-google-fonts/tiny5'
 import { Button, TextInput } from 'react-native-web'
 import { getUser, getUserSummaries } from '../api/tetrioEndpoints'
 import image from '../../assets/tetrioBackground.jpg'
 
 export default function App({ navigation }) {
+  const [fontsLoaded] = useFonts({
+    Tiny5_400Regular
+  })
+
   return (
     <ImageBackground source={image} style={{
       width: '100%',
       height: '100%'
     }}>
       <View style={styles.container}>
-          <Text style={styles.title}>Tetrio Comparator</Text>
+          <Text style={styles.title} numberOfLines={2}>Tetrio Comparator</Text>
           <TextInput
             style={styles.textInput}
-            placeholder='user name'
-            placeholderTextColor={'#808080'}
+            placeholder='username'
+            placeholderTextColor={'#949493'}
           />
-          <View style={styles.spacerUser}/>
-          <Button
-            title='USER INFO'
-            style={[styles.button, { marginTop: 60 }]}
-            onPress={() => {
-              alert("Has perdido")
-            }}
-          />
-          <View style={styles.spacer}/>
-          <View style={styles.userComparator}>
-            <TextInput
-            style={styles.textInput}
-              placeholder='user1 name'
-              placeholderTextColor={'#808080'}
+            <View style={styles.spacerUser}/>
+            <Button
+              title='USER INFO'
+              style={{ marginTop: 60 }}
+              color='#D54DBB'
+              onPress={() => {
+                alert("Has perdido")
+              }}
             />
-            <TextInput
+            <View style={styles.spacer}/>
+
+              <TextInput
               style={styles.textInput}
-              placeholder='user2 name'
-              placeholderTextColor={'#808080'}
+                placeholder='username 1'
+                placeholderTextColor={'#949493'}
+              />
+              <TextInput
+                style={styles.textInput}
+                placeholder='username 2'
+                placeholderTextColor={'#808080'}
+              />
+              <View style={styles.spacerUser}/>
+            <Button
+              title='COMPARE USERS'
+              color='#D54DBB'
+              onPress={() => {
+                alert("Has perdido otra vez")
+              }}
             />
-          </View>
-          <Button
-            title='COMPARE USERS'
-            style={styles.button}
-            onPress={() => {
-              alert("Has perdido otra vez")
-            }}
-          />
         <StatusBar style="dark" />
       </View>
     </ImageBackground>
@@ -52,10 +59,10 @@ export default function App({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
     alignItems: 'center',
-    paddingTop: 80
+    paddingTop: 100
   },
   userComparator: {
     margin: 20
@@ -68,6 +75,8 @@ const styles = StyleSheet.create({
     fontSize: '528%',
     fontWeight: 'bold',
     marginBottom: 40,
+    textAlign: 'center',
+    fontFamily: 'Tiny5_400Regular'
   },
   buttonGroup: {
     alignItems: 'center',
@@ -78,13 +87,11 @@ const styles = StyleSheet.create({
   },
   textInput: {
     backgroundColor: 'white',
+    fontSize: 25,
     marginTop: 10,
-    height: 25,
-    width: 300,
+    height: '5%',
+    width: '60%',
     textAlign: 'center',
-    borderRadius: 40,
+    borderRadius: 5
   },
-  button: {
-    marginTop: 10
-  }
 });
