6cc9edef40938e0ed694ffa62f00c2c27fdc396e
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-07-09 00:57:33 +0200

    Refactor UserScreen to fetch and display user data with error handling

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,22 +1,54 @@
-import { StyleSheet, Text, ImageBackground } from 'react-native'
+import { StyleSheet, Text, ImageBackground, View } from 'react-native'
 import { useFonts } from 'expo-font'
 import image from '../../assets/tetrioBackground.jpg'
 import { Tiny5_400Regular } from '@expo-google-fonts/tiny5'
+import { useEffect, useState } from 'react'
+import { showMessage } from 'react-native-flash-message'
+import { getUser, getUserSummaries } from '../api/tetrioEndpoints'
+import { FlatList } from 'react-native-web'
 
 
 export default function UserScreen({ navigation, route }) {
-    const [fontsLoaded] = useFonts({
-        Tiny5_400Regular
-    })
-    return (
-      <ImageBackground source={image} style={{
-            width: '100%',
-            height: '100%'
-          }}>
-        <Text style={styles.title} numberOfLines={2}>Tetrio Comparator</Text>
-      </ImageBackground>
+  const [user, setUser] = useState({})
+  const [fontsLoaded] = useFonts({
+      Tiny5_400Regular
+  })
 
-    );
+  useEffect(() => {
+    fetchUser()
+  }, [route])
+
+  const renderUser = ({ item }) => {
+    <Text>{ item.username }</Text>
+  }
+
+  const fetchUser = async () => {
+    try {
+      const fetchedUser = await getUserSummaries(route.params.name)
+      setUser(fetchedUser)
+    } catch (error) {
+      showMessage({
+        message: `There was an error while retrieving user: ${error}`,
+        type: 'error',
+
+      })
+    }
+  }
+
+
+  return (
+    <ImageBackground source={image} style={{
+          width: '100%',
+          height: '100%'
+        }}>
+      <FlatList
+        contentContainerStyle={styles.container}
+        ListHeaderComponent={<Text style={styles.title} numberOfLines={2}>Tetrio Comparator</Text>}
+        data={user}
+      />
+    </ImageBackground>
+
+  );
 }
 
 const styles = StyleSheet.create({
