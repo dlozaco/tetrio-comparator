a621c42e6b39af4afe7cfd34f3ba13ba99d84aaa
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-07-09 12:40:21 +0200

    Add Maven wrapper and initial project structure with Spring Boot setup

---
+++ src/backend/TetrioComparator/src/main/java/tetrioComparator/TetrioComparatorApplication.java
@@ -0,0 +1,13 @@
+package tetrioComparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetrioComparatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TetrioComparatorApplication.class, args);
+    }
+
+}
---
+++ src/backend/TetrioComparator/src/test/java/tetrioComparator/TetrioComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package tetrioComparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrioComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
